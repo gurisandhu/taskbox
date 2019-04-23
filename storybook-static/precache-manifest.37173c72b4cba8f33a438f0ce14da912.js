0b346156a9ac60471187f9e05fda7c7ce897f81b
Guri Sandhu <contactguri@gmail.com>
2019-04-23 16:35:56 +1000

    taskbox UI

---
+++ storybook-static/precache-manifest.37173c72b4cba8f33a438f0ce14da912.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d2fb22b1afc6ce0609edd8858cb9773",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.589e3c5d3fc39b74072f.bundle.js"
+  },
+  {
+    "revision": "0fef8c91828ce5ac9682",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.589e3c5d3fc39b74072f.bundle.js"
+  },
+  {
+    "url": "vendors~main.589e3c5d3fc39b74072f.bundle.js"
+  }
+]);
